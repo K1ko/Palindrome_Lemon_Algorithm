33accbf5194c8b86a52e51d70164b56b42dc07d5
K1ko <80548418+K1ko@users.noreply.github.com>
2022-09-22 00:05:47 +0200

    Add files via upload
    The algorithm that can be read as a palindrome.

---
+++ Lemon_executable.py
@@ -0,0 +1,13 @@
+
+s = input()#
+p = s[::-1]#
+if s == p:#
+    print("yes")#    
+else:#
+    print("no")#    
+    #)"on"(tnirp    
+#:esle
+    #)"sey"(tnirp    
+#:p == s fi
+#]1-::[s = p
+#)(tupni = s
